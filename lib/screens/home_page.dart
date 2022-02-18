cbe9706469c2f2e0bae2e10e6d92c9b5757fa728
KamoliddinAkromov <akromovk7@gamil.com>
2022-02-18 21:04:04 +0500

    Home Page Yasash Boshlandi

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,4 @@
-import 'package:exam/models/userdata.dart';
-import 'package:exam/screens/widgets/my_app_bar.dart';
+import 'package:exam/core/constants/const.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -14,13 +13,46 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          MyAppBar(
-            title: ("Hello ${Userdata.currentUser.name}"),
-            image: 'assets/images/appbarimage.png',
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * 0.55,
+                  color: Colors.blueGrey,
+                  child: Image.asset("assets/images/appbarimage.png",
+                      fit: BoxFit.cover),
+                ),
+                Positioned(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 260, left: 30),
+                        child: Text(
+                          "Where are you",
+                          style: TextStyle(
+                              color: ConsColors.white,
+                              fontSize: FontsizeConst.ExtraLargeSize,
+                              fontWeight: FontweightConst.bold),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 30),
+                        child: Text("going next?",style: TextStyle(
+                          color: ConsColors.white,
+                          fontSize: FontsizeConst.ExtraLargeSize,
+                          fontWeight: FontweightConst.bold),),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/screens/widgets/my_app_bar.dart
+++ lib/screens/widgets/my_app_bar.dart
@@ -1,12 +1,12 @@
 import 'package:exam/core/constants/const.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class MyAppBar extends StatelessWidget with PreferredSizeWidget {
   final String? title;
   final Color? color;
   final String? image;
-  const MyAppBar({Key? key, this.title, this.color, this.image})
+  final String? subtitle;
+  const MyAppBar({Key? key, this.subtitle, this.title, this.color, this.image})
       : super(key: key);
 
   @override
@@ -23,19 +23,36 @@ class MyAppBar extends StatelessWidget with PreferredSizeWidget {
           bottomRight: Radius.circular(33.0),
         ),
       ),
-      leading: IconButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        icon: SvgPicture.asset("assets/icons/arrow-left.svg"),
-        iconSize: FontsizeConst.LargeSize,
-        color: ConsColors.Black,
-      ),
+      // leading: IconButton(
+      //   onPressed: () {
+      //     Navigator.pop(context);
+      //   },
+      //   icon: SvgPicture.asset("assets/icons/arrow-left.svg"),
+      //   iconSize: FontsizeConst.LargeSize,
+      //   color: ConsColors.Black,
+      // ),
       elevation: 0,
-      title: Text(
-        title ?? "",
-        style: TextStyle(color: color ?? ConsColors.Black),
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 200, left: 30, right: 35),
+            child: Column(
+              children: [
+                Text(
+                  title ?? "",
+                  style: TextStyle(
+                      color: color ?? ConsColors.white,
+                      fontSize: FontsizeConst.LargeSize,
+                      fontWeight: FontweightConst.bold),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
+      toolbarHeight: 300,
+      actions: [CircleAvatar(backgroundImage: AssetImage(image!),)],
       flexibleSpace: FlexibleSpaceBar(
         background: Container(
           decoration: BoxDecoration(
